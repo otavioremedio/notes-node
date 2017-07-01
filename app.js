a808ab7bbbf2e2a5ce5a60322e4d931ad7057148
otavioremedio <otavioremedio@gmail.com>
2017-06-30 22:25:21 -0300

    root

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-console.log('Starting app.');
+console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
+const notes = require('./notes.js');
 
-var user = os.userInfo();
+console.log(_.isString(true));
+console.log(_.isString('Teste'));
 
-fs.appendFile('greetings.txt', `E ae ${user.username}!`);
+var filteredArray = _.uniq(['Teste','Teste',2,35]);
+console.log(filteredArray);
+
+
+//console.log('Result:', notes.add(9, -2));
+//var user = os.userInfo();
+//fs.appendFile('greetings.txt', `E ae ${user.username}! You are ${notes.age}.`);
+//npm init
