2045947fa22f93f3822c5cdbe6d7775320f0200d
Otavio Remedio <otavioremedio@gmail.com>
2017-07-12 09:16:30 -0300

    modulo 3 aula 14

---
+++ playground/debugging.js
@@ -0,0 +1,22 @@
+var person = {
+  name: 'Otavio'
+};
+
+person.age = 25;
+
+
+//breakpoint
+debugger;
+
+person.name = 'Mike';
+
+console.log(person);
+
+
+//node debug
+//n proxima execução
+//repl acessar o debug e alterar/verificar variaveis
+
+
+//node inspector
+//instalar node debuuger no atom
